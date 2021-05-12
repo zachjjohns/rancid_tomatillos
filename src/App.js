a1f10418097e960dfdfe5ae82cd266e9ee1c32f9
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-12 19:54:34 -0400

    Add navbar function component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import movieData from './movieData';
 import Movies from './Movies';
+import NavBar from './NavBar';
+import './App.css';
 
 
 export default class App extends Component {
@@ -13,6 +15,9 @@ export default class App extends Component {
   render() {
     return (
       <main>
+        <header>
+          <NavBar/>
+        </header>
         <Movies movies={this.state.movies}/>
       </main>
     )
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,7 +3,7 @@ import './MovieCard.css';
 
 export default function MovieCard({ img, title, id}) {
   return (
-    <div class="movie-card" id={id}>
+    <div className="movie-card" id={id}>
       <img alt=" " src={img}></img>
       <h3>{title}</h3>
     </div>
