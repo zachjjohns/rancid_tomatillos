e4d54bb1eedcb453291dc147dae0d23044a48c84
Zach Johnson <zachjjohns@gmail.com>
2021-05-15 15:57:41 -0500

    Delete comments and refactor

--- src/API.js
+++ src/API.js
@@ -1,18 +1,4 @@
-// const route = "https://rancid-tomatillos.herokuapp.com/api/v2"
-// const checkForError = (response) => {
-//   if (!response.ok) {
-//     throw new Error("Error: Unable to find movie data")
-//   } else {
-//     return response.json();
-//   }
-// }
-
 export const getAllMovies = async() => {
  return (await fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")).json();
-  // if (!response.ok) {
-  //   throw new Error("Error: Unable to find movie data")
-  // }
-  // const movieData = await response.json();
-  // return movieData
 }
 
