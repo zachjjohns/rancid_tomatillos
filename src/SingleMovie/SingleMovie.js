fc34a34d6df1e1dde4ef7fe2916a0f769ac10fa1
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-19 19:10:46 -0400

    Fix issue with double clicking movie to change screen

--- src/SingleMovie/SingleMovie.js
+++ src/SingleMovie/SingleMovie.js
@@ -3,8 +3,9 @@ import './SingleMovie.css';
 import { Link } from 'react-router-dom';
 
 export default function SingleMovie({ movie }) {
-  return (
-    <div>
+  if(movie) {
+    return (
+      <div>
       <div className="banner-container" id={movie.id}>
         <img className="banner-photo" src={movie.backdrop_path} alt={`Background banner for ${movie.title}`}></img>
         <Link to='/' className="return-home"><i className="fas fa-times"></i></Link>
@@ -15,7 +16,7 @@ export default function SingleMovie({ movie }) {
           <h3 className="movie-title">{movie.title}</h3>
           <p className="movie-overview">{movie.overview}</p>
           <p className="movie-release">Release Date: {movie.release_date}</p>
-          <p className="movie-genres">Genres: {movie.genres.map(genre => ` ${genre} |`)}</p>
+          <p className="movie-genres">Genres: {movie.genres}</p>
           <p className="movie-rating">Average Rating: {movie.average_rating}</p>
           <p className="movie-budget">Budget: ${movie.budget}</p>
           <p className="movie-revenue">Revenue: ${movie.revenue}</p>
@@ -23,5 +24,5 @@ export default function SingleMovie({ movie }) {
         </div>
       </div>
     </div>
-  )
+  )}
 }
