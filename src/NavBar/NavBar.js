3ab34ca9ac2f4b6e9dce3e9faebfb8f7a573b4a5
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-21 13:17:57 -0400

    Add 2 props to go on input jsx tag

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,12 +2,12 @@ import React from 'react';
 import "./NavBar.css";
 import { Link } from 'react-router-dom';
 
-export default function NavBar() {
+export default function NavBar({ searchValue, handleChange }) {
   return (
     <header className="navbar">
       <Link to="/">Rotten Tomatillos</Link>
       <div>
-        <input type="search" placeholder="Search Movies"></input>
+        <input type="search" placeholder="Search Movies" value={searchValue} onChange={(event) => handleChange(event)}></input>
         <span>UserName</span>
       </div>
     </header>
