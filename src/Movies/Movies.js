aff345448b937e12e7c6641fd00d35de9d0ca22b
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-22 12:47:50 -0400

    Refactor Movies component

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -2,32 +2,29 @@ import React from 'react';
 import MovieCard from '../MovieCard/MovieCard';
 import './movies.css';
 export default function Movies({ movies, searchValue }) {
-  if(movies){
-    const movieCards = movies.map(movie => {
-      return (
-        <MovieCard
-        id={movie.id}
-        img={movie.poster_path}
-        title={movie.title}
-        key={movie.id}
-        />
-      )
-    })
-    
-    if(searchValue){
-      const filteredMovies = movieCards.filter(movie => movie.props.title.toLowerCase().includes(searchValue.toLowerCase()));
-      return (
-        <main className='movies-display'>
-          {!filteredMovies.length && <h1>It looks like no movie match your search. please try a new search</h1>}
-          {filteredMovies}
-        </main>
-      )}
-
+  const movieCards = movies.map(movie => {
+    return (
+      <MovieCard
+      id={movie.id}
+      img={movie.poster_path}
+      title={movie.title}
+      key={movie.id}
+      />
+    )
+  })
+  
+  if(searchValue){
+    const filteredMovies = movieCards.filter(movie => movie.props.title.toLowerCase().includes(searchValue.toLowerCase()));
     return (
       <main className='movies-display'>
-        {movieCards}
+        {!filteredMovies.length && <h1>It looks like no movie match your search. please try a new search</h1>}
+        {filteredMovies}
       </main>
-    )
-  }
-  return null
+    )}
+
+  return (
+    <main className='movies-display'>
+      {movieCards}
+    </main>
+  )
 }
