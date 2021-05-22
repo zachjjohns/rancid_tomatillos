fe8e63495036d535068df117c962dd2b03a480f6
Zach Johnson <zachjjohns@gmail.com>
2021-05-21 20:13:02 -0500

    Refactor tests for moviesPage

--- cypress/integration/moviesPage.js
+++ cypress/integration/moviesPage.js
@@ -1,26 +1,42 @@
 describe("Movies Page", () => {
   beforeEach(() => {
     cy.visit('/')
-    cy.fixture('movies.json').then(movieData => {
-      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {movies: movieData.movies});
-    })
+    // cy.fixture('movies.json').then(movieData => {
+    //   cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {movies: movieData.movies});
+    // })
   })
 
   it('should render a NavBar component containing the app name', () => {
+    cy.fixture('movies.json').then(movieData => {
+      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {movies: movieData.movies});
+    })
     cy.get('header').contains('Rotten Tomatillos')
   });
 
   it('should render a main section to contain movie card components', () => {
+    cy.fixture('movies.json').then(movieData => {
+      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {movies: movieData.movies});
+    })
     cy.get('main')
   });
 
   it('should have movie cards with the movie poster and title', () => {
+    cy.fixture('movies.json').then(movieData => {
+      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {movies: movieData.movies});
+    })
     cy.get('img') 
       .get('h3')   
   });
 
-  it('should display an error upon a failed fetch request', () => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {error: "Failed to fetch"})
+  it('should display a loading message when retrieving data', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {error: ""})
     cy.visit('/')
+    cy.get('h1').contains("BRB, Going to go hydrate the hamster (His name is Napples)")
+  });
+
+  it('should display an error message upon a failed fetch', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {movies: [], error: "Oops! Failed to get movie data."})
+    cy.visit('/')
+    cy.get('h1')
   });
 })
--- src/App/App.js
+++ src/App/App.js
@@ -19,7 +19,7 @@ export default class App extends Component {
       const fetchedMovies = await getAllMovies();
       this.setState({movies: fetchedMovies.movies});
       } catch (e) {
-        this.setState({error: e.message})
+        this.setState({error: "Oops! Failed to get movie data."})
       }
   }
 
@@ -32,15 +32,20 @@ export default class App extends Component {
   }
 
   render() {
-    if(!this.state.error && !this.state.movies) {
-      return <h1>BRB Going to go hydrate the hamster(His name is Napples)</h1>
+    if (!!this.state.error) {
+      return <h1>{this.state.error}</h1>
     }
+
+    if (!this.state.error && !this.state.movies) {
+      return <h1>BRB, Going to go hydrate the hamster (His name is Napples)</h1>
+    }
+
     return (
       <div className="app">
         <NavBar searchValue={this.state.search} handleChange={this.handleChange} />
           <Switch>
             <Route exact path='/'>
-              {!!this.state.error ? <h2>{this.state.error}</h2> :
+              {!!this.state.error ? <h1>{this.state.error}</h1> :
               <Movies movies={this.state.movies} searchValue={this.state.search}/>}
             </Route>
             <Route exact
